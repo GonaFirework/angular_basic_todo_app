cc74f80dcdd146296041855155299739917620ce
GONA <gona.firework@gmail.com>
2018-04-08 16:07:15 +0900

    Date 표시 1초마다 갱신되도록 수정

--- src/app/todo/todos/todos.component.ts
+++ src/app/todo/todos/todos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Todo} from '../share/todo.model';
+import {interval} from 'rxjs/observable/interval';
 
 @Component({
   selector: 'app-todos',
@@ -11,12 +12,16 @@ export class TodosComponent implements OnInit {
   newText = '';
   todos: Todo[];
   today: Date = new Date();
+  counter = interval(5000);
 
   constructor() {
     this.todos = [
       { done: false, text: '운동하기'},
       { done: true, text: '공부하기'}
     ];
+    this.counter.subscribe(number => {
+      this.today = new Date();
+    });
   }
 
   ngOnInit() {
@@ -31,5 +36,9 @@ export class TodosComponent implements OnInit {
       done: false,
       text: text
     });
+  }
+
+  changeTime(): void {
+
   }
 }
